c969c1740db64f578b784031f3dd7b1dd9c15d41
Eldar JDPTS-33 <eldar.zaripoff@gmail.com>
2023-04-03 10:43:34 +0300

    Third commit

--- src/test/java/MainTests.java
+++ src/test/java/MainTests.java
@@ -3,6 +3,8 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -19,11 +21,33 @@ public class MainTests {
     }
 
     @Test
+    public void testTaxEarningsMinusSpendingWithHamcrest() {
+        int earnings = 1000;
+        int spendings = 100;
+        int expectedTax = 135;
+
+        int taxEmS = (earnings - spendings) * 15 / 100;
+
+        assertThat(expectedTax, equalTo(taxEmS));
+    }
+    @Test
     public void testForNull() {
 
         assertThrows(NullPointerException.class,()->{
             throw new NullPointerException();
         });
+    }
+
+    @Test
+    public void testForNullWithHamcrest() {
+        String input = "";
+        assertThat(input, isEmptyOrNullString());
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {20, 200, 2000})
+    public void testTaxEarningsWithHamcrest(int earning) {
+        assertThat(earning, greaterThanOrEqualTo(0));
     }
 
     @ParameterizedTest
